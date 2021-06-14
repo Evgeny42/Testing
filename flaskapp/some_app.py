5945be2776e7c01a0dc35c582054675a5f34665f
Evgeny42 <85866969+Evgeny42@users.noreply.github.com>
2021-06-14 18:29:49 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -24,9 +24,9 @@ app = Flask(__name__)
 def hello():
     return " <html><head></head><body>Hello World!</body></html>"
   
-@app.route("/pic")
-def pic():
-    return " <html><head></head><body><img src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=6&amp;m=825383494&amp;s=612x612&amp;w=0&amp;h=pamh6qxyNPCnNAVru4BrAHt2qTHAGCD9lDiN_6MbaNY=" alt="66,296 Struggle Stock Photos, Pictures &amp;amp; Royalty-Free Images - iStock" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 217.784px; margin: 0px;"></body></html>"  
+# @app.route("/pic")
+# def pic():
+#     return " <html><head></head><body><img src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=6&amp;m=825383494&amp;s=612x612&amp;w=0&amp;h=pamh6qxyNPCnNAVru4BrAHt2qTHAGCD9lDiN_6MbaNY=" alt="66,296 Struggle Stock Photos, Pictures &amp;amp; Royalty-Free Images - iStock" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 383px; height: 217.784px; margin: 0px;"></body></html>"  
   
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
