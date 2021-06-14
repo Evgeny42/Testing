36189e60f33707b9ab912c1dff123a1a3548be9a
Evgeny42 <85866969+Evgeny42@users.noreply.github.com>
2021-06-14 18:15:22 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -22,7 +22,11 @@ app = Flask(__name__)
 # декоратор для вывода страницы по умолчанию
 @app.route("/")
 def hello():
-    return " <html><head></head> <body>Hello World!</body></html>"
+    return " <html><head></head><body>Hello World!</body></html>"
+  
+@app.route("/pic")
+def pic():
+    return " <html><head></head><body><img src="./static/picture.jpg"></body></html>"  
   
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
