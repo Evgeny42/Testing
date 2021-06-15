e058e205ca3c5e2fb0cc40b97c137ee2091cdf4d
Evgeny42 <85866969+Evgeny42@users.noreply.github.com>
2021-06-15 16:11:03 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -46,7 +46,7 @@ bootstrap = Bootstrap(app)
 def change_pic(path, value):
     im = Image.open(path)
     x, y = im.size
-    im[:,:,0] = 0
+    im = [:][:] * [1.5, 1,1]
     im.save(path)
 
 @app.route("/", methods=['GET', 'POST'])
