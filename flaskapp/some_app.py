f28f202a506e8c4bd56bc782318d3424a70f749a
Evgeny42 <85866969+Evgeny42@users.noreply.github.com>
2021-06-16 12:52:52 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -47,7 +47,7 @@ def change_pic(path, value):
     fig = plt.figure(figsize=(6, 4))
     ax = fig.add_subplot()
     gr_path = "./static/pictures/picture.png"
-#     plt.savefig(gr_path)
+    plt.savefig(gr_path)
 #     plt.close()
     
     im = Image.open(path)
