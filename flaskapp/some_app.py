facdd1c9dd036f69336f8bfe19486a9121afdc30
Evgeny42 <85866969+Evgeny42@users.noreply.github.com>
2021-06-17 22:09:34 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -99,6 +99,9 @@ def problem(path, value):
             avrgColor[i] = round(np.sum(arr[:,:,i].mean()))
         fig2, ax2 = plt.subplots()
         ax2.bar(colorList, avrgColor)
+	# Устанавливаем точность деления на осях
+        ax2.yaxis.set_major_locator(ticker.MultipleLocator(5))
+        ax2.yaxis.set_minor_locator(ticker.MultipleLocator(1))
 	
         ax2.set_facecolor('floralwhite')
         fig2.set_facecolor('seashell')
