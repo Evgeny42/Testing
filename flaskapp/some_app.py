a3b4bf906423a9f6151b55fcdc2bab11d082463f
Evgeny42 <85866969+Evgeny42@users.noreply.github.com>
2021-06-22 19:16:39 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -44,74 +44,98 @@ app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
 bootstrap = Bootstrap(app)
 
 def problem(path, color1, color2, color3):
+    # Открываем изображение по указанному пути
     im = Image.open(path)
-    
-        # Заменяем символы r,g,b на соответствующие индексы
-    value = [color1, color2, color3]
+    # В charValue запишем символы цветов (первая буква)
+    # В intValue запишем номер цвета 
+    charValue = [color1, color2, color3]
+    intValue = [0,0,0]
+
     # Сохраняем размерность картинки
     x,y = im.size
-    # сохраняем картинку в виде массива numpy
+    # Сохраняем картинку в виде массива numpy
     arr = np.asarray(im)
+    # С помощью цикла запишем сумму каждого цвета
+    # в заданном порядке
     eachColorSum = [0,0,0]
     for i in range(3):
-        value[i].lower()
-        if "r" in value[i]:
-            value[i] = 0
+        charValue[i].lower()
+        if "r" in charValue[i]:
+            intValue[i] = 0
             eachColorSum[0] = np.sum(arr[:,:,i])
-        elif "g" in value[i]:
-            value[i] = 1
+        elif "g" in charValue[i]:
+            intValue[i] = 1
             eachColorSum[1] = np.sum(arr[:,:,i])
-        elif "b" in value[i]:
-            value[i] = 2
+        else:
+            intValue[i] = 2
             eachColorSum[2] = np.sum(arr[:,:,i])
 
-    colorSum = eachColorSum[0] + eachColorSum[1] + eachColorSum[2]
+    summ = eachColorSum[0] + eachColorSum[1] + eachColorSum[2]
+    # В данном массиве запишем соотношение цвета в процентах
     colorPercent = [0,0,0]
     for i in range (3):
         # Прописываем условие, чтобы избежать ошибки деления на 0
-            if colorSum != 0:
-                    colorPercent[i] = eachColorSum[i] / colorSum * 100
-    fig1, ax1 = plt.subplots()
+        if summ != 0:
+            colorPercent[i] = eachColorSum[i] / summ * 100
+
+    fig, ax = plt.subplots()
     # Используем гистограмму
     # Передаем название для каждой (цвет)
     # и его соответствующее значение
     colorList = ["red", "green", "blue"]
-    ax1.bar(colorList, colorPercent)
-    # Устанавливаем цвет графика
-    ax1.set_facecolor('seashell')
-    fig1.set_facecolor('floralwhite')
-    fig1.set_figwidth(6)  #  ширина фигуры
-    fig1.set_figheight(4)  #  высота фигуры
-# Сохраняем фигуру
+    ax.bar(colorList, colorPercent)
     plt.savefig("./static/images/myFig1.png")
     plt.close()
 
+    # Инициализируем массивы средних 
+    # значений по вертикали и горизонтали
+    averByHoriz = [[0 for i in range(x)] for j in range(3)]
+    averByVert  = [[0 for i in range(y)] for j in range(3)]
 
-    # Средний цвет
-    avrgColor = [0,0,0]
-    # Заполняем среднимим значениями
+    fig, ax = plt.subplots()
+    # Записываем средние значения
+    # по горизонтали
     for i in range (3):
-        avrgColor[i] = round(np.sum(arr[:,:,i].mean()))
-    fig2, ax2 = plt.subplots()
-    ax2.bar(colorList, avrgColor)
-
-    ax2.set_facecolor('floralwhite')
-    fig2.set_facecolor('seashell')
-    fig2.set_figwidth(6)  #  ширина фигуры
-    fig2.set_figheight(4)  #  высота фигуры
-
-    plt.savefig("./static/images/avrg.png") 
+        for j in range(0, x):
+            averByHoriz[i][j] = round(np.sum(arr[:,j,i].mean()))
+    
+    lin = np.linspace(0, 10, x)
+    # Добавляем необходимые значения в график
+    for i in range (3):
+        ax.plot(lin, averByHoriz[i], color=charValue[i])
+    plt.savefig("./static/images/horizontal.png")
     plt.close()
 
 
+    fig, ax = plt.subplots()
+    # Записываем средние значения
+    # по вертикали
+    for i in range (3):
+        for j in range(0, y):
+            averByVert[i][j] = round(np.sum(arr[j,:,i].mean()))
+        
+    lin = np.linspace(0, 10, y)
+    # Добавляем необходимые значения в график
+    for i in range (3):
+        ax.plot(lin, averByVert[i], color=charValue[i])
+    plt.savefig("./static/images/vertical.png")
+    plt.close()
 
-# Проходясь по картинке изменяем цвета пикселей 
-# в зависимости от выбранного порядка цветовых карт
-    for i in range(0,y):
-        for j in range(0,x):
-            im.putpixel((j,i),(arr[i][j][int(value[0])],arr[i][j][int(value[1])],arr[i][j][int(value[2])]))
-        # Сохраняем изображение
-    im.save(path)
+    
+    pixelMap = im.load()
+
+    # Получаем новое изображение на основе "im"
+    img = Image.new(im.mode, im.size)
+    pixelsNew = img.load()
+    # Заменяем цветовые карты в зависимости от введенного порядка
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            # На каждом цикле получаем цвета пикселя
+            myList = list(pixelMap[i,j])
+            # И наконец, применяем цвет по порядку
+            pixelsNew[i,j] = (myList[intValue[0]], 
+                 myList[intValue[1]], myList[intValue[2]])
+    img.save(path)
     
 @app.route("/", methods=['GET', 'POST'])
 def main():
@@ -119,15 +143,17 @@ def main():
     imagePath = None
     graphPath1 = None
     graphPath2 = None
+    graphPath3 = None
     if form.validate_on_submit():
         photo = form.upload.data.filename.split('.')[-1]
         imagePath = os.path.join('./static/images', f'photo.{photo}')
         graphPath1 = os.path.join('./static/images', f'myFig1.png')
-        graphPath2 = os.path.join('./static/images', f'avrg.png')
+        graphPath2 = os.path.join('./static/images', f'horizontal.png')
+        graphPath3 = os.path.join('./static/images', f'vertical.png')
         # Сохраняем наше загруженное изображение
         form.upload.data.save(imagePath)
         problem(imagePath, form.color1.data, form.color2.data, form.color3.data)
-    return render_template('main.html', form=form, image=imagePath, graph1=graphPath1, graph2=graphPath2)
+    return render_template('main.html', form=form, image=imagePath, graph1=graphPath1, graph2=graphPath2, graph3=graphPath3)
 # Запускаем наше приложение
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
